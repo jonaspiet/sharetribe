f6a945623da8939658e4bd5f6ea5984da5000020
Vesa Luusua <vesa.luusua@gmail.com>
2015-02-26 15:28:40 +0200

    community_translations migration and model translation from varchar to text schema version change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150311073502) do
+ActiveRecord::Schema.define(:version => 20150311100232) do
 
   create_table "auth_tokens", :force => true do |t|
     t.string   "token"
@@ -274,6 +274,15 @@ ActiveRecord::Schema.define(:version => 20150311073502) do
     t.datetime "expires_at"
     t.datetime "created_at",                  :null => false
     t.datetime "updated_at",                  :null => false
+  end
+
+  create_table "community_translations", :force => true do |t|
+    t.integer  "community_id",                  :null => false
+    t.string   "locale",          :limit => 16, :null => false
+    t.string   "translation_key",               :null => false
+    t.text     "translation"
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
   end
 
   create_table "contact_requests", :force => true do |t|
